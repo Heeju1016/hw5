0ca974bd22e3165dafc47c9c65c164e3bce3380c
Heeju1016 <162046940+Heeju1016@users.noreply.github.com>
2024-05-20 18:58:46 +0900

    Add files via upload

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,34 @@
+
+import React, { useState } from 'react'
+import AddBtn from '../assets/img/add.svg'
+
+const TodoList = () => {
+    const [ inputValue, setInputValue] = useState('');
+    const [ todo, setTodo ] = useState( [
+        
+    ])
+    const createTodo = (e) => {
+        e.preventDefault()
+        if(inputValue === '') {
+            alert("내용을 입력해주세요!");
+        } else { 
+            setTodo([...todo, inputValue]);
+        }
+    }
+  return (
+    <div className='wrap'>
+        <form onSubmit={createTodo}>
+            <h1>Todo List</h1>
+            <input type='text' placeholder='입력하세요' onChange={(e) => {setInputValue(e.target.value)}}></input>
+            <button><img src={AddBtn}/></button>
+        </form>
+        <ul>
+            {todo.map((item)=> {
+                return <li>{item}</li>
+            })}
+        </ul>
+    </div>
+  )
+}
+
+export default TodoList
